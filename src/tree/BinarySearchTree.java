3952429fa101551cb13810cde3b5b8a0e5ca2717
Nikolin Majmari <nikolinmajmari30@gmail.com>
2025-04-07 09:35:47 +0200

    fixes

--- src/tree/BinarySearchTree.java
+++ src/tree/BinarySearchTree.java
@@ -49,12 +49,40 @@ public class BinarySearchTree<T extends Comparable<T>> implements Iterable<T> {
 
         return false;
     }
-
+    public boolean containsIteratively(T value) {
+        Node<T>current = root;
+        if(current==null){
+            return false;
+        }
+        while(current!=null){
+            if(current.data.equals(value)){
+                return true;
+            }
+            if(current.data.compareTo(value)<0){
+                current = current.right;
+            }
+            else if(current.data.compareTo(value)>0){
+                current = current.left;
+            }
+        }
+        return false;
+    }
     public boolean contains(T value) {
         return contains(root,value);
     }
     public boolean contains(Node<T> root, T value) {
-        //
+        if(root==null){
+            return false;
+        }
+        if(root.data.equals(value)){
+            return true;
+        }
+        if(root.data.compareTo(value)<0){
+            return contains(root.right,value);
+        }
+        if(root.data.compareTo(value)>0){
+            return contains(root.left,value);
+        }
         return false;
     }
 
@@ -62,31 +90,154 @@ public class BinarySearchTree<T extends Comparable<T>> implements Iterable<T> {
         return min(root);
     }
     private T min(Node<T> root){
-        return null;
+        if(root==null){
+            return null;
+        }
+        while(root.left!=null){
+            root = root.left;
+        }
+        return root.data;
     }
     public T max(){
         return max(root);
     }
     private T max(Node<T> root){
-        return null;
+        if(root==null){
+            return null;
+        }
+        if(root.right == null){
+            return root.data;
+        }
+        return max(root.right);
     }
 
     public void remove(T value) {
         root = remove(root,value);
     }
     private Node<T> remove(Node<T> root, T value) {
-        return null;
+        if(root==null){
+            return null;
+        }
+        if(!root.data.equals(value) ){
+            if(root.data.compareTo(value) < 0){
+                root.right = remove(root.right,value);
+            }
+            if(root.data.compareTo(value) > 0){
+                root.left = remove(root.left,value);
+            }
+            return root;
+        }
+        if(root.data.equals(value)) {
+            if (root.left == null && root.left == null) {
+                return null;
+
+            }
+            if(root.left == null) {
+                return root.right;
+            }
+            if(root.right == null) {
+                return root.left;
+            }
+            root.data = min(root.right);
+            root.right = remove(root.right,root.data);
+            return root;
+
+        }
+        return root;
     }
 
     public int distance(T left, T right) {
-        return 0;
+        if(left.compareTo(right) > 0){
+            return distance(right, left);
+        }
+        Node<T> closestCommonAncestor = closestCommonAncestor(root,left,right);
+
+        int distance = hops(closestCommonAncestor, left) + hops(closestCommonAncestor, right);
+
+        return distance;
+    }
+
+    public Node<T> closestCommonAncestor(Node<T> root, T left, T right) {
+        if(root == null) {
+            return null;
+        }
+
+        if(root.data.compareTo(left) > 0 && root.data.compareTo(right) < 0) {
+            return root;
+        }
+        if(root.data.compareTo(right) > 0 && root.data.compareTo(left) > 0) {
+            return closestCommonAncestor(root.left, left, right);
+        }
+        if(root.data.compareTo(right) < 0 && root.data.compareTo(left) < 0) {
+            return closestCommonAncestor(root.right, left, right);
+        }
+
+        return null;
+    }
+
+    public int hops(Node<T> root, T value) {
+        if(root==null){
+            return 0;
+        }
+
+        int counter = 0;
+        while(root != null) {
+            if(root.data.equals(value)){
+                return counter;
+            }
+            if(root.data.compareTo(value)<0){
+                root = root.right;
+                counter++;
+            }
+            if(root.data.compareTo(value)>0){
+                root = root.left;
+                counter++;
+            }
+        }
+
+        return -1;
+    }
+
+    public void calculateHeight(Node <T> root) {
+        if(root == null){
+            return;
+        }
+        calculateHeight(root.left);
+        calculateHeight(root.right);
+        if(root.left==null && root.right==null){
+            root.height = 1;
+        }else if(root.left==null){
+            root.height = root.right.height+1;
+        }else if(root.right==null){
+            root.height = root.left.height+1;
+        }else{
+            root.height = Math.max(root.left.height+1,root.right.height+1);
+        }
+    }
+    public int height(Node<T> root){
+        if(root==null){
+            return 0;
+        }
+        return 1 + Math.max(height(root.left),height(root.right));
     }
 
     public int rootBalanceFactor(){
-        return 0;
+        root.balanceFactor =  height(root.left) - height(root.right);
+        return root.balanceFactor;
+    }
+
+    public void calculateBalanceFactor(Node<T> root){
+        if(root==null){
+            return;
+        }
+        root.balanceFactor = root.left.height - root.right.height;
+        calculateBalanceFactor(root.left);
+        calculateBalanceFactor(root.right);
     }
 
     public void calculateAllBalanceFactors(){
+        calculateHeight(root);
+        calculateBalanceFactor(root);
     }
 
 
--- src/tree/Node.java
+++ src/tree/Node.java
@@ -4,6 +4,8 @@ public class Node<T> {
     T data;
     Node<T> left;
     Node<T> right;
+    public int balanceFactor;
+    public int height;
     public Node(T data) {
         this.data = data;
     }
