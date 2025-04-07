19838380f2de366ec0a37fc54b97e9a6e65a893b
Nikolin Majmari <nikolinmajmari30@gmail.com>
2025-04-07 06:27:01 +0200

    removed solutions

--- src/tree/BinarySearchTree.java
+++ src/tree/BinarySearchTree.java
@@ -46,37 +46,15 @@ public class BinarySearchTree<T extends Comparable<T>> implements Iterable<T> {
     }
 
     private boolean isValid(Node<T> root) {
-        if (root == null) {
-            return true;
-        }
-        if (root.left == null && root.right == null) {
-            return true;
-        }
-        if(root.left!=null && max(root.left).compareTo(root.data)>0){
-            return false;
-        }
-        if(root.right!=null && min(root.right).compareTo(root.data)<0){
-            return false;
-        }
-        return isValid(root.left) && isValid(root.right);
+
+        return false;
     }
 
     public boolean contains(T value) {
         return contains(root,value);
     }
     public boolean contains(Node<T> root, T value) {
-        if (root == null) {
-            return false;
-        }
-        if (root.data.equals(value)) {
-            return true;
-        }
-        if (root.data.compareTo(value) < 0) {
-            return contains(root.right,value);
-        }
-        else if (root.data.compareTo(value) > 0) {
-            return contains(root.left,value);
-        }
+        //
         return false;
     }
 
@@ -84,48 +62,20 @@ public class BinarySearchTree<T extends Comparable<T>> implements Iterable<T> {
         return min(root);
     }
     private T min(Node<T> root){
-        if(root==null){
-            return null;
-        }
-        if(root.left==null){
-            return root.data;
-        }
-        return min(root.left);
+        return null;
     }
     public T max(){
         return max(root);
     }
     private T max(Node<T> root){
-        if(root==null){
-            return null;
-        }
-        if(root.right==null){
-            return root.data;
-        }
-        return max(root.right);
+        return null;
     }
 
     public void remove(T value) {
-
+        root = remove(root,value);
     }
     private Node<T> remove(Node<T> root, T value) {
-        if (root != null) {
-            if (root.data.equals(value)) {
-                if(root.left==null){
-                    return root.right;
-                } else if (root.right==null) {
-                    return root.left;
-                }else{
-                    root.data = min(root.right);
-                    root.right = remove(root.right,root.data);
-                }
-            } else if (root.data.compareTo(value) < 0) {
-                root.left = remove(root.left,value);
-            }else{
-                root.right = remove(root.right,value);
-            }
-        }
-        return root;
+        return null;
     }
 
     public int distance(T left, T right) {
